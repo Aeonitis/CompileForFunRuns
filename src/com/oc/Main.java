05b1e8a4fcd3ec9bb275265ae2cae4e4394109cb
Aeonitis <aeonitis@gmail.com>
2017-09-20 15:19:13 +0100

    Initial Commit

---
+++ src/com/oc/Main.java
@@ -0,0 +1,12 @@
+package com.oc;
+
+import com.oc.associate.studyguide.Try;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Try tryOut = new Try();
+        tryOut.tryThis();
+    }
+}
---
+++ src/com/oc/associate/studyguide/Try.java
@@ -0,0 +1,11 @@
+package com.oc.associate.studyguide;
+
+/**
+ * Try out experiments here
+ */
+public class Try {
+
+    public void tryThis() {
+        System.out.println("LET'S BEGIN!");
+    }
+}
