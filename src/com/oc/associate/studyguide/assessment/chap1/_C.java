f992631afb55c0b985f89246cf6e69623e6245b5
Aeonitis <aeonitis@gmail.com>
2017-09-20 20:13:20 +0100

    Chapter 1 assessment questions added (part 2)

--- src/com/oc/associate/studyguide/assessment/chap1/_C.java
+++ src/com/oc/associate/studyguide/assessment/chap1/_C.java
@@ -4,13 +4,13 @@ package com.oc.associate.studyguide.assessment.chap1;
  * Q:1
  * Identifiers (Letter, Underscore, Dollar Sign) are ok.
  * Class & Instance variables e.g. '$' have default values and allow referencing
- * Problem: local variable 'a_b' not initialized, needs assignment, and defaults to null. Won't compile
+ * Problem: local variable 'a_b' not initialized, needs assignment, and defaults to null. Won't compile, although if a_b was an instance variable, the code would compile and output '0null'
  */
 public class _C {
     private static int $;
-
+    static String a_b;
     public static void main(String[] main) {
-        String a_b;
+
         System.out.print($);
         System.out.print(a_b);
     }
