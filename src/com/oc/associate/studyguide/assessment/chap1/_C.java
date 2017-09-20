7a69d8e77ab022f3593534de25a1996abfac9b38
Aeonitis <aeonitis@gmail.com>
2017-09-20 20:32:59 +0100

    Chapter 1 assessment questions added (part 4)

--- src/com/oc/associate/studyguide/assessment/chap1/_C.java
+++ src/com/oc/associate/studyguide/assessment/chap1/_C.java
@@ -2,7 +2,7 @@ package com.oc.associate.studyguide.assessment.chap1;
 
 /**
  * Q:1
- * Identifiers (Letter, Underscore, Dollar Sign) are ok.
+ * Identifiers (Letter, Underscore, Currency Symbols e.g. $/฿/﷼/₽/€/£/¥/₪) are ok. Others which won't work are \.[]{}()*+-?^$|
  * Class & Instance variables e.g. '$' have default values and allow referencing
  * Problem: local variable 'a_b' not initialized, needs assignment, and defaults to null. Won't compile, although if a_b was an instance variable, the code would compile and output '0null'
  */
